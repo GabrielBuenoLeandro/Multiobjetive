229ef31349f768abd989534460a1d021ad12c57c
Samir Martins <milani.martins@gmail.com>
2023-05-05 14:00:21 -0300

    Adjusting variables names (work in progress...)

--- sysidentpy/multiobjective/multiobjective_parameter_estimation.py
+++ sysidentpy/multiobjective/multiobjective_parameter_estimation.py
@@ -51,14 +51,14 @@ class IM(FROLS):
                  final_model=np.zeros((1, 1)),
                  w=np.zeros((1, 1))
                  ):
-        self.Static_Gain = static_gain
-        self.Static_Function = static_function
+        self.static_gain = static_gain
+        self.static_function = static_function
         self.psi = psi
         self._n_inputs = n_inputs
         self.non_degree = non_degree
         self.model_type = model_type
-        self.Y_static = y_static
-        self.X_static = x_static
+        self.y_static = y_static
+        self.x_static = x_static
         self.final_model = final_model
         self.gain = gain
         self.y_train = y_train
@@ -97,31 +97,31 @@ class IM(FROLS):
                     if k + 1 == qit[i, j]:
                         N_aux[i, k] = 1 + N_aux[i, k]
         qit = N_aux
-        Q = np.zeros((len(self.Y_static), len(qit)))
-        for i in range(0, len(self.Y_static)):
+        Q = np.zeros((len(self.y_static), len(qit)))
+        for i in range(0, len(self.y_static)):
             for j in range(0, len(qit)):
-                Q[i, j] = self.Y_static[i, 0]**(qit[j, 0])
+                Q[i, j] = self.y_static[i, 0] ** (qit[j, 0])
                 for k in range(0, self._n_inputs):
-                    Q[i, j] = Q[i, j]*self.X_static[i, k]**(qit[j, 1+k])
+                    Q[i, j] = Q[i, j] * self.x_static[i, k] ** (qit[j, 1 + k])
         return Q.dot(R) 
 
     def static_gain(self):
         """Matrix of static regressors referring to derivative."""
         R, qit = self.r_qit()
-        H = np.zeros((len(self.Y_static), len(qit)))
-        G = np.zeros((len(self.Y_static), len(qit)))
-        for i in range(0, len(self.Y_static)):
+        H = np.zeros((len(self.y_static), len(qit)))
+        G = np.zeros((len(self.y_static), len(qit)))
+        for i in range(0, len(self.y_static)):
             for j in range(1, len(qit)):
-                if self.Y_static[i, 0] == 0:
+                if self.y_static[i, 0] == 0:
                     H[i, j] = 0
                 else:
-                    H[i, j] = self.gain[i]*qit[j, 0]*self.Y_static[i, 0]\
+                    H[i, j] = self.gain[i]*qit[j, 0]*self.y_static[i, 0]\
                         **(qit[j, 0]-1)
                 for k in range(0, self._n_inputs):
-                    if self.X_static[i, k] == 0:
+                    if self.x_static[i, k] == 0:
                         G[i, j] = 0
                     else:
-                        G[i, j] = qit[j, 1+k]*self.X_static[i, k]\
+                        G[i, j] = qit[j, 1+k]*self.x_static[i, k]\
                             **(qit[j, 1+k]-1)
         return (G+H).dot(R)
     
@@ -138,7 +138,7 @@ class IM(FROLS):
                     a1.append(w1[i])
                     a2.append(w2[j])
                     a3.append(1 - (w1[i]+w2[j]))
-        if self.Static_Gain != False and self.Static_Function != False:
+        if self.static_gain != False and self.static_function != False:
             w = np.zeros((3, len(a1)))
             w[0, :] = a1
             w[1, :] = a2
@@ -175,13 +175,13 @@ class IM(FROLS):
             part1 = w[0, i]*(self.psi).T.dot(self.psi)
             part2 = w[0, i]*(self.psi.T).dot(self.y_train)
             w = 1
-            if self.Static_Function == True:
+            if self.static_function == True:
                 QR = self.static_function()
                 part1 = w[w, i]*(QR.T).dot(QR) + part1
-                part2 = part2 + (w[w, i]*(QR.T).dot(self.Y_static))\
+                part2 = part2 + (w[w, i] * (QR.T).dot(self.y_static))\
                     .reshape(-1,1)
                 w = w + 1
-            if self.Static_Function == True:
+            if self.static_function == True:
                 HR = self.static_gain()
                 part1 = w[w, i]*(HR.T).dot(HR) + part1
                 part2 = part2 + (w[w, i]*(HR.T).dot(self.gain)).reshape(-1,1)
@@ -193,12 +193,12 @@ class IM(FROLS):
             J[0, i] = (((self.y_train)-(self.psi.dot(Theta))).T).dot((self.y_train)\
                      -(self.psi.dot(Theta)))
             w = 1
-            if self.Static_Gain == True:
+            if self.static_gain == True:
                 J[w, i] = (((self.gain)-(HR.dot(Theta))).T).dot((self.gain)\
                           -(HR.dot(Theta)))
                 w = w+1
-            if self.Static_Function == True:
-                J[w, i] = (((self.Y_static)-(QR.dot(Theta))).T).dot((self.Y_static)-(QR.dot(Theta)))
+            if self.static_function == True:
+                J[w, i] = (((self.y_static) - (QR.dot(Theta))).T).dot((self.y_static) - (QR.dot(Theta)))
         for i in range(0, np.shape(w)[1]):
             E[i] = np.linalg.norm(J[:, i]/np.max(J))
         return J/np.max(J), w, E, Array_theta, HR, QR
